58084df60be52e7e1e1a12fba504750e8a9af5c9
gazz31 <wojtekku@outlook.com>
2018-06-03 22:31:35 +0200

    newer

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Page Title</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	</head>
+
+	<body>
+        
+        <div class="main_box">
+            <nav>
+                <a name="x4" class="one" href="#x">mój wzór</a>
+                <a class="one" href="#x1">ciepłe barwy</a>
+                <a class="one" href="#x2">kwiat</a>
+                <a class="one" href="#x3">czyste niebo</a>
+            </nav>
+            <header>
+                
+                <a name="x"><div class="img"></div></a>
+                <a name="x1"><div class="img_1"></div></a>
+                 <a name="x2"><div class="img_2"></div></a>
+                <a name="x3"><div class="img_3"></div></a>
+                <a href="#x4"><button class="but">Powrót</button></a>
+            </header>
+            
+        </div>
+	</body>
+</html>
